ec8c7da0846a31d10224cedfdd36d29ec4711526
Jean Verster <jeanverster0107@gmail.com>
2022-02-22 10:57:15 +0100

    remove lib from git

--- libraries/components/lib/Box/Box.js
+++
@@ -1,7 +0,0 @@
-import React, { forwardRef } from "react";
-import { Box as MUIBox } from "@mui/material";
-const Box = forwardRef((props, ref) => {
-    return (React.createElement(MUIBox, Object.assign({ ref: ref }, props), props.children));
-});
-Box.displayName = "Box";
-export { Box };
